210b6145b9552065c41e96ece48a47b374a36c08
Miroslav P. Marinov <miroslav12122@gmail.com>
2014-09-03 23:42:14 +0300

    Initial commit
    The problem is solved using C#

---
+++ 02.ABCheck/ABCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+    class ABCheck
+    {
+        static bool AbCheck(string input)
+        {
+            bool answer = false; 
+            for (int i = 0; i < input.Length - 4; i++)
+            {
+                if (input[i] == 'a' && input[i + 4] == 'b')
+                {
+                    answer = true;
+                }
+            }
+            return answer;
+        }
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            bool finalAnswer = AbCheck(input);
+            Console.WriteLine(finalAnswer);
+        }
+    }
+
